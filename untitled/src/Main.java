40997019e4a15421a3ae8ba27fb55ebf51ea3d3f
Anna Kuznetsova <akry67@gmail.com>
2021-10-08 19:05:32 +0400

    rubles

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+    public static void main(String[] args) {
+        long initialAccount = 100;
+        long depositAmount = 2000;
+        long bonusRuble = depositAmount / 100;
+        if (depositAmount >= 1000) {
+            bonusRuble = depositAmount / 100;
+        }
+        if (depositAmount < 1000) {
+            bonusRuble = 0;
+        }
+        long finalAccount = initialAccount + depositAmount + bonusRuble;
+        System.out.println(finalAccount);
+        System.out.println(bonusRuble);
+    }
 }
